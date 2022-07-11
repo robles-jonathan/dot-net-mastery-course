9a86c6d21c20df84aba0a3da35886ca9e9d7ea56
Jonathan Robles <jonathan.robles287@gmail.com>
2022-07-11 16:15:36 -0500

    Create Property in Page Model

---
+++ AbbyWeb/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,14 @@
+using AbbyWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        public Category Category { get; set; }
+        public void OnGet()
+        {
+        }
+    }
+}
